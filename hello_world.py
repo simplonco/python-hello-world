f43f137b11d05c6d2ec8cf69addf0a5bed49eb46
adora84 <abdulkader.kalaaji@gmail.com>
2016-06-01 14:45:04 +0200

    adora test1

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,7 @@
 #
 # Skeleton file for the Python "Hello World" exercise.
 #
-
-
-  def hello(name=''):
+def hello(name=''):
     if name:
-    return "Hello, %s!" % name
-     return "Hello, World!"
+        return "Hello, %s!" % name
+    return "Hello, World!"
